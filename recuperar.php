fe6885d22cdcbb4ee92b2446e48f8b8ed883e3bc
mora8292 <alexmartincarrillo19@hotmail.com>
2026-08-04 16:55:42 -0600

    reportes prueba

--- recuperar.php
+++ recuperar.php
@@ -1,80 +1,101 @@
 <?php
+
 require 'conexion.php';
 session_start();
 
 date_default_timezone_set("America/Mexico_City");
 
-$fecha_tolerancia = date("Y-m-d H:i:s", strtotime("-5 minutes"));
+// Mostrar eventos del día en adelante
+$fecha_tolerancia = date("Y-m-d 00:00:00");
 
-$verTodos = isset($_POST['verTodos']) ? $_POST['verTodos'] : 0;
+$verTodos = isset($_POST['verTodos']) ? intval($_POST['verTodos']) : 0;
 
-if (isset($_SESSION["usuario"]["matricula"]) && !empty($_SESSION["usuario"]["matricula"])) {
+echo "<option value=''>Seleccione un evento</option>";
+
+if(isset($_SESSION["usuario"]["matricula"])){
 
     $matricula = $_SESSION["usuario"]["matricula"];
 
-    // Obtener la carrera del estudiante
-    $sqlCarrera = "SELECT c.carrera
-                   FROM estudiantes e
-                   INNER JOIN carreras c ON e.carrera = c.id
-                   WHERE e.matricula = ?";
+    $sql = "SELECT c.carrera
+            FROM estudiantes e
+            INNER JOIN carreras c
+            ON e.carrera=c.id
+            WHERE e.matricula=?";
 
-    $stmtCarrera = $mysqli->prepare($sqlCarrera);
-    $stmtCarrera->bind_param("s", $matricula);
-    $stmtCarrera->execute();
+    $stmt = $mysqli->prepare($sql);
 
-    $resultadoCarrera = $stmtCarrera->get_result();
-    $datosCarrera = $resultadoCarrera->fetch_assoc();
+    if(!$stmt){
+        die($mysqli->error);
+    }
 
-    $stmtCarrera->close();
+    $stmt->bind_param("s",$matricula);
+    $stmt->execute();
 
-    $carrera = $datosCarrera['carrera'];
+    $res = $stmt->get_result();
 
-    if ($verTodos == 1) {
+    if($res->num_rows==0){
+        exit;
+    }
 
-        // Mostrar todos los eventos
-        $consulta = "SELECT *
-                     FROM eventos
-                     WHERE Fecha_Evento >= ?
-                     ORDER BY Fecha_Evento ASC";
+    $datos = $res->fetch_assoc();
 
-        $stmt = $mysqli->prepare($consulta);
-        $stmt->bind_param("s", $fecha_tolerancia);
+    $stmt->close();
 
-    } else {
+    $carrera = $datos["carrera"];
 
-        // Mostrar únicamente los eventos de la carrera
-        $consulta = "SELECT *
-                     FROM eventos
-                     WHERE `$carrera` = 1
-                     AND Fecha_Evento >= ?
-                     ORDER BY Fecha_Evento ASC";
+    if($verTodos){
 
-        $stmt = $mysqli->prepare($consulta);
-        $stmt->bind_param("s", $fecha_tolerancia);
+        $sql="SELECT *
+              FROM eventos
+              WHERE Fecha_Evento>=?
+              ORDER BY Fecha_Evento ASC";
+
+        $stmt=$mysqli->prepare($sql);
+
+        $stmt->bind_param("s",$fecha_tolerancia);
+
+    }else{
+
+        $sql="SELECT *
+              FROM eventos
+              WHERE `$carrera`=1
+              AND Fecha_Evento>=?
+              ORDER BY Fecha_Evento ASC";
+
+        $stmt=$mysqli->prepare($sql);
+
+        if(!$stmt){
+            die($mysqli->error);
+        }
+
+        $stmt->bind_param("s",$fecha_tolerancia);
 
     }
 
     $stmt->execute();
-    $resultado = $stmt->get_result();
 
-    while($fila = $resultado->fetch_assoc()){
-        echo "<option value='".$fila['Id_Evento']."'>".$fila['Nombre_Evento']."</option>";
-    }
-
-    $stmt->close();
+    $resultado=$stmt->get_result();
 
 }else{
 
-    // Coordinadores
-    $consulta = "SELECT *
-                 FROM eventos
-                 ORDER BY Fecha_Evento ASC";
-
-    $resultado = $mysqli->query($consulta);
-
-    while($fila = $resultado->fetch_assoc()){
-        echo "<option value='".$fila['Id_Evento']."'>".$fila['Nombre_Evento']."</option>";
-    }
+    $resultado=$mysqli->query("
+        SELECT *
+        FROM eventos
+        ORDER BY Fecha_Evento ASC
+    ");
 
 }
-?>
+
+if($resultado && $resultado->num_rows>0){
+
+    while($fila=$resultado->fetch_assoc()){
+
+        echo "<option value='".$fila["Id_Evento"]."'>".$fila["Nombre_Evento"]."</option>";
+
+    }
+
+}else{
+
+    echo "<option value=''>No hay eventos disponibles</option>";
+
+}
--- reportes.php
+++ reportes.php
@@ -1,172 +1,215 @@
 <?php
-  session_start();
-  if (($_SESSION["usuario"]["clave_C"])!= ''){
+session_start();
+if (($_SESSION["usuario"]["clave_C"]) != '') {
 ?>
 
-<html>
-<head> 
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="estilos.css">	
-  <script src="js/jquery.js"></script>
-  <script src="alertify/alertify.min.js"></script>
-  <link rel="stylesheet" href="alertify/css/alertify.min.css" />
-  <link rel="stylesheet" href="alertify/css/themes/default.min.css" />
-                
+  <html>
 
-	<title> Reportes Excel y PDF	 </title>
-</head>
-<body>
-  <section>
-    <div class="container-longer">
-      <div class="row">
-        <div class="col-lg-3 col-sm-3 imagen">
-          <img class="img img-fluid" src="imagenes/itesa.png" width="150px" height="60px">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <script src="js/jquery.js"></script>
+    <script src="alertify/alertify.min.js"></script>
+    <link rel="stylesheet" href="alertify/css/alertify.min.css" />
+    <link rel="stylesheet" href="alertify/css/themes/default.min.css" />
+
+
+    <title> Reportes Excel y PDF </title>
+  </head>
+
+  <body>
+    <section>
+      <div class="container-longer">
+        <div class="row">
+          <div class="col-lg-3 col-sm-3 imagen">
+            <img class="img img-fluid" src="imagenes/itesa.png" width="150px" height="60px">
+          </div>
+
+          <div class="col-lg-6 col-sm-6 sup">
+            <p style="text-align: center; margin-top: 30px; color: black;"><b>INSTITUTO TECNOLÓGICO SUPERIOR DEL ORIENTE DEL ESTADO DE HIDALGO </b></p>
+          </div>
+
+          <div class="col-lg-3 col-sm-3 imagen">
+            <img class="img img-fluid" src="imagenes/tec.png" width="150px" height="60px">
+          </div>
+
         </div>
-          
-        <div class="col-lg-6 col-sm-6 sup">
-          <p style="text-align: center; margin-top: 30px; color: black;"><b>INSTITUTO TECNOLÓGICO SUPERIOR DEL ORIENTE DEL ESTADO DE HIDALGO </b></p>
+      </div>
+
+      <div class="container-longer">
+
+        <div class="row">
+          <div class="col-lg-12 col-sm-12" style="height: 40px;">
+          </div>
         </div>
 
-        <div class="col-lg-3 col-sm-3 imagen">
-          <img class="img img-fluid" src="imagenes/tec.png"  width="150px" height="60px">
-        </div>
+        <div class="row">
+          <div class="col-lg-3 col-sm-3 ">
+          </div>
 
-      </div> 
-    </div>
+          <div class="col-lg-6 col-sm-6 ">
+            <div class="row">
+              <div class="col-lg-4">
+                <h3>
+                  Selecciona el evento
+                </h3>
+                <form action="PDF_Excel.php" method="post">
+                  <select id="Eventos" name="Eventos" style="width: 150px; height: 30px; text-align: center;">
+                  </select>
 
-    <div class="container-longer">
-
-      <div class="row">
-     	 	<div class="col-lg-12 col-sm-12" style="height: 40px;">	 				
-      	</div>
-   		</div>
-  
-      <div class="row">
-        <div class="col-lg-3 col-sm-3 ">
-        </div> 
-          
-        <div class="col-lg-6 col-sm-6 ">
-          <div class="row">
-            <div class="col-lg-4">
-              <h3>
-                Selecciona el evento
-              </h3>
-              <form action="PDF_Excel.php" method="post">
-                <select id="Eventos" name="Eventos" style="width: 150px; height: 30px; text-align: center;">    
-                </select>
-              
-                <div class="row">
-                  <div class="col-lg-12 col-sm-12" style="height: 120px;">	 				
+                  <div class="row">
+                    <div class="col-lg-12 col-sm-12" style="height: 120px;">
+                    </div>
                   </div>
-                </div>
-             
-          
-                <button class="btn btn-primary btn-lg btn_login" onclick="regresa()" style="padding-left: 2.5rem; padding-right: 2.5rem; text-align: center;" type="button">
-                  <img class=" img-fluid" height="30px" src="imagenes/regresa.png" width="25px" style="text-align: center;">
-                  </img>
-                  Regresar
-                </button>
-            </div>
+
+
+                  <button class="btn btn-primary btn-lg btn_login" onclick="regresa()" style="padding-left: 2.5rem; padding-right: 2.5rem; text-align: center;" type="button">
+                    <img class=" img-fluid" height="30px" src="imagenes/regresa.png" width="25px" style="text-align: center;">
+                    </img>
+                    Regresar
+                  </button>
+              </div>
               <div class="col-lg-2">
               </div>
               <div class="col-lg-6">
                 <div class="row">
-                  <button id="excel" name="excel" type="submit" class="btn btn_coordinador btn-secondary me-md-2 btn-g" type="button" ><img class=" img-fluid" src="imagenes/excel.png" width="40px" height="50px"> </img> <p><b>Excel</b></p></button>	 				
+                  <button id="excel" name="excel" type="submit" class="btn btn_coordinador btn-secondary me-md-2 btn-g" type="button"><img class=" img-fluid" src="imagenes/excel.png" width="40px" height="50px"> </img>
+                    <p><b>Excel</b></p>
+                  </button>
                 </div>
-                <div  class="col-lg-12 col-sm-12" style="height: 10px;">	 				
+                <div class="col-lg-12 col-sm-12" style="height: 10px;">
                 </div>
                 <div class="row">
-                <button id="pdf" name="pdf" type="submit" class="btn btn_coordinador btn-secondary me-md-2 btn-g" type="button"><img class=" img-fluid" src="imagenes/pdf.png" width="40px" height="50px"> </img> <p><b>PDF</b></p></button>	 				
+                  <button id="pdf" name="pdf" type="submit" class="btn btn_coordinador btn-secondary me-md-2 btn-g" type="button"><img class=" img-fluid" src="imagenes/pdf.png" width="40px" height="50px"> </img>
+                    <p><b>PDF</b></p>
+                  </button>
                 </div>
                 <br>
                 <div class="row">
-                <button id="reporte" name="reporte" type="submit" class="btn btn_coordinador btn-secondary me-md-2 btn-g" type="button"><img class=" img-fluid" src="imagenes/reporte.png" width="40px" height="50px"> </img> <p><b>  Reporte</b></p></button>	 				
+                  <button id="reporte" name="reporte" type="submit" class="btn btn_coordinador btn-secondary me-md-2 btn-g" type="button"><img class=" img-fluid" src="imagenes/reporte.png" width="40px" height="50px"> </img>
+                    <p><b> Reporte</b></p>
+                  </button>
                 </div>
-              </form>
+                </form>
+              </div>
             </div>
+
           </div>
-        
+
+          <div class="col-lg-3 col-sm-3 " id="resultado">
+
+          </div>
         </div>
+      </div>
 
-        <div class="col-lg-3 col-sm-3 " id="resultado">
-          
-        </div>
-      </div> 
-    </div>
-  
-  </section>
-</body>
-<script>
-  $.ajax({type: "POST",
-    url: "recuperar.php",
-      success: function(res){
-        $("#Eventos").html(res);
-      }
-  });
+    </section>
+  </body>
+  <script>
+    $(document).ready(function() {
+      cargarEventos();
+    });
 
-  /*
-  $("#excel").click(function(){
+    function cargarEventos() {
 
-    var evento = $("#Eventos").val();
+      $("#Eventos").html("<option>Cargando eventos...</option>");
 
-    $.ajax({type: "POST",
-        url: "excel.php",
-        data: {evento: evento},
-       success: function(res){
-          alertify.success('Se ha creado el documento en Excel :D',10)
-          $("#resultado").html(res);
-          //Creación de modak para avisar que ya descargo xd 
+      $.ajax({
+        url: "recuperar.php",
+        type: "POST",
+        cache: false,
+        timeout: 10000,
+
+        success: function(res) {
+
+          res = $.trim(res);
+
+          if (res.length > 0) {
+            $("#Eventos").html(res);
+          } else {
+            $("#Eventos").html("<option value=''>No hay eventos disponibles</option>");
+          }
+
+        },
+
+        error: function(xhr, status, error) {
+
+          console.log(xhr.responseText);
+
+          $("#Eventos").html("<option value=''>Error al cargar eventos</option>");
+
+          alertify.error("No fue posible obtener los eventos");
+
         }
-      });
-  });
-  */
-   
 
-  function regresa() {
-        history.go(-1);
+      });
+
     }
 
-</script>
-     
-<footer  style="width:100%; margin-left: 0px;">
-<div class="copyright" style="background-color: black;">
-    <div class="container-fluid" style="background-color: black; color: #bbdefb;">
-      <div class="row">
-        <div class="col-lg-4 col-sm-4" style="text-align:center;">
-           <b><br>Página Oficial: itesa.edu.mx</b> 
-        </div>
-        <div class="col-lg-4 col-sm-4" style="text-align:center;">
-          <b> <br>Teléfono: 01 748-912-4450</b>
-        </div>
-        <div class="col-lg-4 col-sm-4" >
-          <center><img src="imagenes/hidalgo.png"  class="img-fluid" alt="Sample " id="img-pie"></center>
+    /*
+    $("#excel").click(function(){
+
+      var evento = $("#Eventos").val();
+
+      $.ajax({type: "POST",
+          url: "excel.php",
+          data: {evento: evento},
+         success: function(res){
+            alertify.success('Se ha creado el documento en Excel :D',10)
+            $("#resultado").html(res);
+            //Creación de modak para avisar que ya descargo xd 
+          }
+        });
+    });
+    */
+
+
+    function regresa() {
+      history.go(-1);
+    }
+  </script>
+
+  <footer style="width:100%; margin-left: 0px;">
+    <div class="copyright" style="background-color: black;">
+      <div class="container-fluid" style="background-color: black; color: #bbdefb;">
+        <div class="row">
+          <div class="col-lg-4 col-sm-4" style="text-align:center;">
+            <b><br>Página Oficial: itesa.edu.mx</b>
+          </div>
+          <div class="col-lg-4 col-sm-4" style="text-align:center;">
+            <b> <br>Teléfono: 01 748-912-4450</b>
+          </div>
+          <div class="col-lg-4 col-sm-4">
+            <center><img src="imagenes/hidalgo.png" class="img-fluid" alt="Sample " id="img-pie"></center>
+          </div>
         </div>
       </div>
     </div>
-</div>
-</footer>
-<style type="text/css">
+  </footer>
+  <style type="text/css">
     html {
-  min-height: 100%;
-  position: relative;
-}
-body {
-  margin: 0;
-  margin-bottom: 80px;
-}
-footer {
-  background-color: black;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  color: white;
-}
-</style>
-</html>
+      min-height: 100%;
+      position: relative;
+    }
+
+    body {
+      margin: 0;
+      margin-bottom: 80px;
+    }
+
+    footer {
+      background-color: black;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      color: white;
+    }
+  </style>
+
+  </html>
 
 <?php
-}else{
-header("Location: index.php");
+} else {
+  header("Location: index.php");
 }
 ?>
